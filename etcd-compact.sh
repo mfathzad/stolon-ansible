3937e7d1f79ebfe3d3fa87b20c413cbd84f85ea7
alireza ehsani <a_ehsani@arsh.co>
2024-08-28 11:58:59 +0330

    refactor playbook

--- etcd-compact.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rev=$(ETCDCTL_API=3 etcdctl --endpoints=192.168.78.14:2379 endpoint status --write-out="json" | egrep -o '"revision":[0-9]*' | egrep -o '[0-9].*')
-ETCDCTL_API=3 etcdctl --endpoints=192.168.78.14:2379 compact $rev
-ETCDCTL_API=3 etcdctl --endpoints=192.168.78.14:2379 defrag
-ETCDCTL_API=3 etcdctl --endpoints=192.168.78.14:2379 alarm disarm
-
